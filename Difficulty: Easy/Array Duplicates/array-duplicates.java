ec9a3ac2f37cf409353998ffa8d5baed335c6ac6
Raje2433 <163415339+Raje2433@users.noreply.github.com>
2025-08-10 12:03:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Array Duplicates/array-duplicates.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+class Solution {
+    public ArrayList<Integer> findDuplicates(int[] arr) {
+        // code here
+        Arrays.sort(arr);
+        ArrayList<Integer> a=new ArrayList<>();
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i]==arr[i+1]){
+                a.add(arr[i]);
+            }
+        }
+        return a;
+    }
+}
